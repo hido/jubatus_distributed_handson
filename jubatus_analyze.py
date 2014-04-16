6720316487e47c4d21f1442d475cbbfe6d7cee28
Shohei Hido <hido@preferred.jp>
2014-04-17 06:14:10 +0900

    Modify scripts for anomaly

--- jubatus_analyze.py
+++ jubatus_analyze.py
@@ -1,13 +1,58 @@
 #!/usr/bin/env python
 
 import sys
-from nearest_neighbor import client
-from nearest_neighbor import types
+import pika
+import json
+import msgpackrpc
+from anomaly import client
+from anomaly import types
+from jubatus.common.datum import Datum
 
-NAME = "sensor_nn"
-nn = client.nearest_neighbor("127.0.0.1", 9199)
-id_ = sys.argv[1]
 
-print nn.similar_row_from_id(NAME, str(id_) , 5)
-    
+NAME = "jubatus_anomaly"
 
+#See http://jubat.us/ja/faq_rpc_err_workaround.html
+def analyze_jubatus(datum_):
+    anomaly_client = client.Anomaly("127.0.0.1", 9199, NAME)
+    retry_max = 1
+    retry_interval = 1
+    score = -1.0
+    try:
+        retry_count = retry_max
+        while True:
+            try:
+                score = anomaly_client.calc_score(datum_)
+                break
+            except (msgpackrpc.error.TransportError, msgpackrpc.error.TimeoutError) as e:
+                retry_count -= 1
+                if retry_count <= 0:
+                    raise
+
+                anomaly.get_client().close()
+                anomaly = client.Anomaly("127.0.0.1", 9199, NAME)
+
+                time.sleep(retry_interval)
+                continue
+
+    except msgpackrpc.error.RPCError as e:
+        raise
+
+    finally:
+        anomaly_client.get_client().close()
+
+    return score
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+   host=sys.argv[1], credentials = pika.PlainCredentials(sys.argv[2], sys.argv[2])))
+channel = connection.channel()
+channel.queue_declare(queue=sys.argv[3])
+
+def callback(ch, method, properties, body):
+    id_, val_ = json.loads(body)
+    # id_ not used
+    datum_ = Datum({"x":val_["x"],"y":val_["y"], "z":val_["z"]})
+    score = analyze_jubatus(datum_)
+    print score
+
+channel.basic_consume(callback, queue='sensor', no_ack=True)
+channel.start_consuming()
--- jubatus_update.py
+++ jubatus_update.py
@@ -4,29 +4,30 @@ import sys
 import pika
 import json
 import msgpackrpc
-from nearest_neighbor import client
-from nearest_neighbor import types
+from anomaly import client
+from anomaly import types
+from jubatus.common.datum import Datum
 
-NAME = "sensor_nn"
+NAME = "jubatus_anomaly"
 
 #See http://jubat.us/ja/faq_rpc_err_workaround.html
-def update_jubatus(id_, datum_):
-    nn = client.nearest_neighbor("127.0.0.1", 9199)
+def update_jubatus(datum_):
+    anomaly_client = client.Anomaly("127.0.0.1", 9199, NAME)
     retry_max = 1
     retry_interval = 1
     try:
         retry_count = retry_max
         while True:
             try:
-                nn.set_row(NAME, str(id_) , datum_)
+                id = anomaly_client.add(datum_)
                 break
             except (msgpackrpc.error.TransportError, msgpackrpc.error.TimeoutError) as e:
                 retry_count -= 1
                 if retry_count <= 0:
                     raise
 
-                nn.get_client().close()
-                nn = client.nearest_neighbor("127.0.0.1", 9199)
+                anomaly.get_client().close()
+                anomaly = client.Anomaly("127.0.0.1", 9199, NAME)
 
                 time.sleep(retry_interval)
                 continue
@@ -35,7 +36,7 @@ def update_jubatus(id_, datum_):
         raise
 
     finally:
-        nn.get_client().close()
+        anomaly_client.get_client().close()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
    host=sys.argv[1], credentials = pika.PlainCredentials('jubatus','jubatus')))
@@ -44,14 +45,9 @@ channel.queue_declare(queue='sensor')
 
 def callback(ch, method, properties, body):
     id_, val_ = json.loads(body)
-    datum = types.datum([], [
-           ["x", val_["x"]],
-           ["y", val_["y"]],
-           ["z", val_["z"]]
-         ])
-    update_jubatus(id_, datum)
-    print datum
+    # id_ not used
+    datum_ = Datum({"x":val_["x"],"y":val_["y"], "z":val_["z"]})
+    update_jubatus(datum_)
 
 channel.basic_consume(callback, queue='sensor', no_ack=True)
 channel.start_consuming()
-
